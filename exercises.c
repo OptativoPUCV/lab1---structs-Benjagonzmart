32be2f6dc5f396b7b29a82f5e843ed8690734911
Your Name <you@example.com>
2024-03-14 21:18:38 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -71,8 +71,6 @@ int *filterEvenNumbers(int arr[], int size, int *newSize)
       newArr[j++] = arr[i];
   *newSize = contador;
   return newArr;
-
-  
 }
 
 /*
@@ -82,7 +80,23 @@ ordenados de menor a mayor y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado de menor a mayor.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-                       int result[]) {}
+                       int result[]) 
+{
+  int i = 0, j = 0, k = 0;
+  while (i < size1 && j < size2)
+    {
+      if (arr1[i] < arr2[j])
+      {
+         result[k++] = arr1[i++];
+      }
+      else
+        
+    }  
+  
+  
+}
+
+
 
 /*
 Ejercicio 5: Comprobación de Ordenación
